968b928b6581b3f192036f2420acc134bae1e145
freemt <yucongo+fmt@gmail.com>
2022-04-11 23:05:18 +0800

    Update 4tabs, cb_params

--- pyvizbee/__main__.py
+++ pyvizbee/__main__.py
@@ -20,54 +20,50 @@ from typing import List
 
 import cchardet as chardet
 
-# import numpy as np
+import numpy as np
 import pandas as pd
 import panel as pn
 import param
 
 # from bokeh.models.widgets.tables import CheckboxEditor, NumberEditor, SelectEditor, TextEditor, StringEditor
 from bokeh.models.widgets.tables import TextEditor
+import holoviews as hv
 
 from unsync import unsync
 
 # import environs
-from icecream import ic  # install as ic_install
+# from icecream import ic  # install as ic_install
 import logzero
 from logzero import logger
 
 from pyvizbee.loglevel import loglevel
 from pyvizbee.loadtext import loadtext
 
+pn.extension("plotly")
+hv.extension("plotly")
+
 logzero.loglevel(loglevel())
 
 logger.debug(" debug: %s", __file__)
 logger.info(" info: %s", __file__)
 
-# ic_install()
-ic.configureOutput(
-    includeContext=True,
-    # outputFunction=logger.info,
-    outputFunction=logger.debug,
-)
-ic.enable()
-# ic.disable()  # to turn off
-
 VIZBEE_DEV = os.environ.get("VIZBEE_DEV")
 if VIZBEE_DEV:
-    from icecream import ic
+    # from icecream import ic
     from add_path import add_path
     add_path([
         r"..\ezbee",
         r"..\cmat2aset",
-        r"..\fastscores",
+        r"..\fast-scores",
     ])
     try:
-        ic(" importing ezbee")
+        logger.debug(" importing ezbee")
         import ezbee
-    except Exception:
-        ic(" import ezbee erro")
+    except Exception as exc:
+        logger.exception(" import ezbee error: %s", exc)
 
-from fast_scores.get_cmat import gen_cmat
+from fast_scores.gen_cmat import gen_cmat
+from cmat2aset import cmat2aset
 
 # pn.config.sizing_mode = "stretch_both"
 pn.config.sizing_mode = "stretch_width"
@@ -79,21 +75,28 @@ file2 = pn.widgets.FileInput(accept=".txt,.csv,.tsv")
 # df: pd.DataFrame = None
 # df_pane: pn.pane.DataFrame = None
 
+# modi
 s_btn_load = pn.widgets.Button(name="Load Files")
+s_btn_params = pn.widgets.Button(name="Parameters")
 s_btn_align = pn.widgets.Button(name="Align")
 s_btn_about = pn.widgets.Button(name="About")
 
+
+DEFAULT_EPS = 10
+DEFAULT_MIN_SAMPLES = 6
 
 # workspace namespace: similar to global()
 ns = SimpleNamespace(**{
     "counter": 0,
     "df": None,
     "df_pane": None,
+    "eps": DEFAULT_EPS,  # Two points are considered neighbors if the distance between the two points is below eps
+    "min_samples": DEFAULT_MIN_SAMPLES,  # The minimum number of neighbors a given point should have in order to be classified as a core point.
 })
 
 about_info = dedent(
     """
-    ## Visbee Aligner
+    ## Vizbee Aligner
 
     from mu's keyborad in cyberspace (join qq group **316287378** or visit the bumblebee forum [bumblebee.freeforums.net](https://bumblebee.freeforums.net/board/1/general-discussion) to be kept informed and for questions and feedback)
     """
@@ -141,7 +144,11 @@ def filevalue2list(value: bytes) -> List[str]:
 
 
 def gen_df():
-    """Gen df (pandas.DataFrame) from file1.value/file2.value."""
+    """Gen df (pandas.DataFrame) from file1.value/file2.value.
+
+    _ =  [elm for elm in ns.df.text1.to_list() if elm.strip()]
+    cmat = gen_cmat(_, ns.df.text2.to_list())
+    """
     # global file1, file2
 
     list1 = filevalue2list(file1.value)
@@ -171,7 +178,6 @@ def cb_submit(event=param.parameterized.Event):
     """Callback for the submit button (upload)."""
     # global df  # , df_pane
     ns.counter += 1
-    # ic(" cb_submit ", ns.counter)
 
     logger.debug("ns.counter: %s", ns.counter)
 
@@ -189,12 +195,20 @@ def cb_submit(event=param.parameterized.Event):
     s_cb_load()
 
 
+def cb_reset(event=param.parameterized.Event):
+    """Callback for the reset button (eps/min_samples)."""
+    logger.debug("cb_reset")
+    logger.info("cb_reset")
+    ns.eps = DEFAULT_EPS
+    ns.min_samples = DEFAULT_MIN_SAMPLES
+    s_cb_params()
+
+
 def s_cb_load(event=param.parameterized.Event):
     """Callback1 for tab1 (submit)."""
     # global df, df_pane
 
     ns.counter += 1
-    # ic("cb_submit", ns.counter)
 
     logger.debug("ns.counter: %s", ns.counter)
 
@@ -218,6 +232,124 @@ def s_cb_load(event=param.parameterized.Event):
 
     return "Page 1"
 
+def s_cb_params(event=param.parameterized.Event):
+    """Callback for tab2 (params).
+
+    int_slider = pn.widgets.IntSlider(name='Integer Slider', start=0, end=8, step=2, value=4)
+
+    int_slider
+
+    https://www.quansight.com/post/panel-holoviews-learning-aid
+    def update(event):
+        if event.obj is radio_button:
+            radio_button_display.object = f'Radio Button Value: {radio_button.value}'
+    radio_button.param.watch(update, "value")
+    """
+    def update(event):
+        if event.obj is w_eps:
+            logger.debug("w_eps.value: %s", w_eps.value)
+            ns.eps = w_eps.value
+        if event.obj is w_min_samples:
+            logger.debug("w_min_samples.value: %s", w_min_samples.value)
+            ns.min_samples = w_min_samples.value
+
+        # update self to update headmaps
+        s_cb_params()
+
+    logger.debug(" s_cb_params ")
+
+    w_eps = pn.widgets.FloatSlider(
+        name='epsilon', start=0, end=20, step=.2, value=ns.eps
+    )
+    w_min_samples = pn.widgets.IntSlider(
+        name='min samples', start=1, end=20, step=1, value=ns.min_samples
+    )
+
+    w_eps.param.watch(update, "value")
+    w_min_samples.param.watch(update, "value")
+
+    xlabel = "x"
+    ylabel = "y"
+
+    _ =  [elm for elm in ns.df.text1.to_list() if elm.strip()]
+    cmat = gen_cmat(_, ns.df.text2.to_list())
+
+    # restore
+    logzero.loglevel(loglevel())
+
+    _ = [[*elm, v] for elm, v in np.ndenumerate(cmat)]
+    hm1 = hv.HeatMap(
+        _,
+        label="(z) likelihood heatmap",
+        name="name1",
+    )
+    hm1.opts(
+        # xticks=None,
+        # tools=['hover'],  # only for bokeh
+        # colorbar=True,
+        # cmap="viridis_r",
+        xlabel=xlabel,
+        ylabel=ylabel,
+        # cmap="viridis_r",
+        # cmap="viridis",
+        cmap="gist_earth_r",
+        # cmap="summer_r",
+        # cmap="fire_r",
+    )
+
+    aset = cmat2aset(
+        cmat,
+        eps=ns.eps,
+        min_samples=ns.min_samples,
+    )
+
+    logzero.loglevel(loglevel())  # fastlid reset logzero.loglevel(20)
+    logger.debug("esp: %s, min-samples: %s", ns.eps, ns.min_samples)
+    logger.debug("aset: \n%s", aset)
+
+    arr = np.array(aset, dtype=object)
+
+    # convert "" in col3 to nan
+    arr[:, 2][arr[:, 2] == ""] = np.nan
+    arr[arr == ""] = 0
+    arr = arr.astype(float)
+
+    # set value to np.nan for "": old way
+    # _ = [elm.tolist() if elm[2] != 0 else elm.tolist()[:2]
+    # + [np.nan] for elm in arr]
+
+    hm2 = hv.HeatMap(
+        arr,
+        label="(z) likelihood align trace",
+    )
+
+    hm2.opts(
+        # xticks=None,
+        # tools=['hover'],  # only for bokeh
+        colorbar=True,
+        # cmap="viridis_r",
+        xlabel=xlabel,
+        ylabel=ylabel,
+        # cmap="viridis_r",
+        # cmap="viridis",
+        cmap="gist_earth_r",
+        # cmap="summer_r",
+        # cmap="fire_r",
+        # labelled=["en", "zh", "llh"]  # ["x", "y", "z"]
+    )
+
+    tab = pn.Column(
+        pn.Row(
+            w_eps,
+            w_min_samples,
+            button_reset,
+        ),
+        pn.Row(hm1 + hm2),
+    )
+    template.main[0].objects = [tab]
+
+    return "Params tab"
+
 
 editors = {
     # "text1": StringEditor(),
@@ -240,7 +372,6 @@ formatters = {
 
 def s_cb_align(event=param.parameterized.Event):
     """Callback2 for align tab."""
-    # ic("cb 2 s_cb_align")
 
     logger.debug(" s_cb_align")
 
@@ -276,21 +407,24 @@ def s_cb_align(event=param.parameterized.Event):
 def s_cb_about(event):
     """Callback for about."""
     ns.counter += 1
-    # ic("cb3 ", ns.counter)
 
     logger.debug(" ns.counter: %s", ns.counter)
 
     template.open_modal()
     time.sleep(10)
 
-    # ic("cb3 done")
     template.close_modal()
 
 
 button_submit = pn.widgets.Button(name="Submit")
 button_submit.on_click(cb_submit)
 
+button_reset = pn.widgets.Button(name="Reset")
+button_reset.on_click(cb_reset)
+
+# modi
 s_btn_load.on_click(s_cb_load)
+s_btn_params.on_click(s_cb_params)
 s_btn_align.on_click(s_cb_align)
 s_btn_about.on_click(s_cb_about)  # modi
 
@@ -306,12 +440,17 @@ ACCENT_COLOR = "#0072B5"
 DEFAULT_PARAMS = {
     # "site": "Panel Multi Page App",
     "site": "Align with Style",
-    "title": "Visbee",
+    "title": "Vizbee",
     "accent_base_color": ACCENT_COLOR,
     "header_background": ACCENT_COLOR,
 }
 template = pn.template.FastListTemplate(
-    sidebar=[s_btn_load, s_btn_align, s_btn_about],  # modi
+    sidebar=[
+        s_btn_load,
+        s_btn_params,
+        s_btn_align,
+        s_btn_about,
+    ],  # modi
     # main=[ishow],
     # main=[page],
     # sidebar=[btn_submit, btn_align],
